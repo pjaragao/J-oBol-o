9b80ad177d8551c0bc50ea816b2e1ea477a3ebac
pjaragao <pjaragaojr@gmail.com>
2026-01-08 11:55:10 -0300

    campanha marketing v3
     correção codigo

--- apps/web/src/app/admin/marketing/components/CampaignForm.tsx
+++ apps/web/src/app/admin/marketing/components/CampaignForm.tsx
@@ -198,7 +198,7 @@ export function CampaignForm() {
                                                 onChange={(e) => setFilters({ ...filters, targetGroupAdmins: e.target.checked })}
                                                 className="sr-only peer"
                                             />
-                                            <div className="w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer dark:bg-slate-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                                            <div className="w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer dark:bg-slate-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                                         </label>
                                     </div>
 
